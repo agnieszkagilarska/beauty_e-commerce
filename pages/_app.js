256d4cbeff245e4a450a436d57afc7fa29a4611d
zuzaflis <zuzaflis20@gmail.com>
2023-01-14 22:52:47 +0100

    layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import React from 'react'
 import '../styles/globals.css'
+import {Layout} from '../components' 
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return(
+ <Layout>
+ <Component {...pageProps} />
+  </Layout>
+  )
 }
+export default MyApp
